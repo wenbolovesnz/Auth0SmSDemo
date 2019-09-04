cb6628ce98651e8b3c89cfed52ebd5c202f95567
wenbo <wenbolovesnz@gmail.com>
2019-09-04 14:34:13 +1200

    first commit

---
+++ src/components/Hero.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Hero = () => (
+  <div className="text-center hero my-5">
+    <h1 className="mb-4">SMS Login Demo Project</h1>
+  </div>
+);
+
+export default Hero;
